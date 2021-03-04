f907a3e61e59889a12f1a6ce6fe6dec7d3c1271f
Davis Nguyen <52183786+DarcXnite@users.noreply.github.com>
2021-03-04 17:52:48 -0600

    Update Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Heading = () => {
-    return (
-        <header className='navbar navbar-light bg-light App-header'>
-            <a href="/pokedex-app" className='pokemon-font navbar-brand mb-0 h1'>Pokedex</a>
-        </header>
-    )
-}
+	return (
+		<header className="navbar navbar-light bg-light App-header">
+			<Link to="/pokedex-app">
+				<h1 href="/pokedex-app" className="pokemon-font navbar-brand mb-0 h1">
+					Pokedex
+				</h1>
+			</Link>
+		</header>
+	);
+};
 
-export default Heading
+export default Heading;
