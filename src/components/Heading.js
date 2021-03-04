790c637f1deef205b1f7c5e3b7088115984756c2
Davis Nguyen <52183786+DarcXnite@users.noreply.github.com>
2021-03-04 17:37:55 -0600

    Update Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = () => {
     return (
         <header className='navbar navbar-light bg-light App-header'>
-            <a href="/" className='pokemon-font navbar-brand mb-0 h1'>Pokedex</a>
+            <a href="/pokedex-app" className='pokemon-font navbar-brand mb-0 h1'>Pokedex</a>
         </header>
     )
 }
